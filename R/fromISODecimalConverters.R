a3d3b434b95cd1953d8a898a06ce43a68b141e68
Graeme Diack <graeme@atlanticsalmontrust.org>
2022-07-04 15:41:47 +0000

    replace built in format check with get_iso11784_format function

--- R/fromISODecimalConverters.R
+++ R/fromISODecimalConverters.R
@@ -3,19 +3,16 @@
 #'
 #'
 #' ISO11784 15 Digit Decimal format To ISO11784 Dot Hexadecimal
-#' @param dec A vector of ISO11784 15 Digit Decimal format strings.
+#' @param .data A vector of ISO11784 15 Digit Decimal format strings.
 #' @return A vector of Dot Hexadecimal format strings.
 #' @export
 #' @examples
 #' ISOdecimalToISOdothex(c('989737733408912'))
-ISOdecimalToISOdothex <- function(dec){
+ISOdecimalToISOdothex <- function(.data){
   out <- c()
-  for(i in dec){
-    if(nchar(i) != 15){
-      warning(paste0("Format does not meet ISO11784/5 standard - Not 15 digits: ",i))
-      out <- append(out,NA)
-    }else if(as.numeric(stringr::str_sub(i,4,15)) > 274877906943){
-      warning(paste0("Format does not meet ISO11784/5 standard - Animal ID exceeds max (274877906943/38bits): ",stringr::str_sub(i,4,15)))
+  for(i in .data){
+    if(ISO11784Tools::get_iso11784_format(i) != 'ISOdecimal'){
+      warning("Unexpected format does not match ISOdecimal")
       out <- append(out,NA)
     }else{
       # convert the input integer to hexadecimal ISO format ABC.1234567ABC
@@ -36,19 +33,16 @@ ISOdecimalToISOdothex <- function(dec){
 }
 
 #' ISO11784 15 Digit Decimal format To ISO11784 Raw Hexadecimal format, animal ID on the LEFT
-#' @param dec A vector of ISO11784 15 Digit Decimal format strings.
+#' @param .data A vector of ISO11784 15 Digit Decimal format strings.
 #' @return A vector of Raw Hexadecimal format strings.
 #' @export
 #' @examples
 #' ISODecimalToISO64bitLeft(c('989737733408912'))
-ISODecimalToISO64bitLeft <- function(dec){
+ISODecimalToISO64bitLeft <- function(.data){
   out <- c()
-  for(i in dec){
-    if(nchar(i) != 15){
-      warning(paste0("Format does not meet ISO11784/5 standard - Not 15 digits: ",i))
-      out <- append(out,NA)
-    }else if(as.numeric(stringr::str_sub(i,4,15)) > 274877906943){
-      warning(paste0("Format does not meet ISO11784/5 standard - Animal ID exceeds max (274877906943/38bits): ",stringr::str_sub(i,4,15)))
+  for(i in .data){
+    if(ISO11784Tools::get_iso11784_format(i) != 'ISOdecimal'){
+      warning("Unexpected format does not match ISOdecimal")
       out <- append(out,NA)
     }else{
       manufacturer <- stringr::str_sub(i,1,3) # split the input value to 3/12
@@ -66,19 +60,16 @@ ISODecimalToISO64bitLeft <- function(dec){
 }
 
 #' ISO11784 15 Digit Decimal format To ISO11784 Raw Hexadecimal format, animal ID on the RIGHT
-#' @param dec A vector of ISO11784 15 Digit Decimal format strings.
+#' @param .data A vector of ISO11784 15 Digit Decimal format strings.
 #' @return A vector of Raw Hexadecimal format strings.
 #' @export
 #' @examples
 #' ISODecimalToISO64bitRight(c('989737733408912'))
-ISODecimalToISO64bitRight <- function(dec){
+ISODecimalToISO64bitRight <- function(.data){
   out <- c()
-  for(i in dec){
-    if(nchar(i) != 15){
-      warning(paste0("Format does not meet ISO11784/5 standard - Not 15 digits: ",i))
-      out <- append(out,NA)
-    }else if(as.numeric(stringr::str_sub(i,4,15)) > 274877906943){
-      warning(paste0("Format does not meet ISO11784/5 standard - Animal ID exceeds max (274877906943/38bits): ",stringr::str_sub(i,4,15)))
+  for(i in .data){
+    if(ISO11784Tools::get_iso11784_format(i) != 'ISOdecimal'){
+      warning("Unexpected format does not match ISOdecimal")
       out <- append(out,NA)
     }else{
       manufacturer <- stringr::str_sub(i,1,3) # split the input value to 3/12
